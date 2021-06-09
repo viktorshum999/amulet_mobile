39293b2484be97e3f80fa0c3e33198326109b652
viktorshum999 <gm64252@gmail.com>
2021-06-09 22:04:44 +0600

    amulets1

--- lib/amulets/amulet3.dart
+++ lib/amulets/amulet3.dart
@@ -1,4 +1,6 @@
+import 'package:amulet_mobile/bottom/matrix/matrixtable.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 import '../main.dart';
 
@@ -7,14 +9,31 @@ class Amulet3 extends StatefulWidget {
   _Amulet3State createState() => _Amulet3State();
 }
 
+final Shader linearGradient = LinearGradient(
+  colors: <Color>[
+    Color(0xFFFFC700),
+    Color(0xFFFFC700),
+    Color(0xFFFEDA5B),
+    Color(0xFFFEE075),
+  ],
+).createShader(Rect.fromLTWH(0.0, 0.0, 200.0, 70.0));
+
 class _Amulet3State extends State<Amulet3> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-            title: Text('ДЕНЕЖНЫЙ АМУЛЕТ'),
+            iconTheme: IconThemeData(
+              color: Color(0xFFFEDA5B), //change your color here
+            ),
+            title: Text(
+              'SEXY CHAKRA',
+              style: TextStyle(
+                  fontFamily: 'TrajanPro3Regular',
+                  foreground: Paint()..shader = linearGradient),
+            ),
             backgroundColor: Colors.transparent,
-            elevation: 0.0,
+            elevation: 0.1,
             centerTitle: true),
         extendBodyBehindAppBar: true,
         body: WillPopScope(
@@ -38,9 +57,12 @@ class _Amulet3State extends State<Amulet3> {
                 radius: 1.0,
                 stops: [0.0, 0.33, 0.65, 1.0],
               )),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+              child: ListView(
+                // mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+                  SizedBox(
+                    height: 30,
+                  ),
                   Table(),
                 ],
               ),
@@ -61,27 +83,146 @@ class _TableState extends State<Table> {
     return Center(
       child: Column(
         children: [
-          IconButton(
-            alignment: Alignment.topRight,
-            iconSize: 30,
-            icon: const Icon(Icons.new_releases),
-            tooltip: 'Increase volume by 10',
-            onPressed: () => ({
-              Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (context) => Navigation())),
-            }),
+          Stack(
+            children: [
+              Container(
+                  decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 3), // changes position of shadow
+                        ),
+                      ],
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      color: Colors.red),
+                  height: 350,
+                  width: 250,
+                  // color: Colors.red,
+                  child: Image(image: AssetImage('images/Frame4.png'))),
+              Positioned(
+                  bottom: 230,
+                  right: 80,
+                  child: Text(
+                    "???".toUpperCase(),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 14.0,
+                    ),
+                  )),
+              Positioned(
+                top: 8,
+                right: 8,
+                child: Container(
+                  height: 30,
+                  width: 30,
+                  margin: EdgeInsets.all(1),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    gradient: LinearGradient(
+                      colors: [
+                        Color(0xFFFFC700),
+                        Color(0xFFFFC700),
+                        Color(0xFFFEDA5B),
+                        Color(0xFFFEE075),
+                      ],
+                      // stops: [0.0, 0.33, 0.65, 1.0],
+                    ),
+                    // shape: BoxShape.rectangle,
+                    boxShadow: <BoxShadow>[
+                      BoxShadow(
+                        color: Color(0x40000000),
+                        offset: Offset(0.0, 4.0),
+                        blurRadius: 4.0,
+                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: IconButton(
+                      icon: SvgPicture.asset(
+                        "assets/Vector.svg",
+                        color: Colors.black,
+                      ),
+                      onPressed: () => ({
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => Navigation())),
+                      }),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-          Container(
-              height: 300,
-              width: 300,
-              child: Image(image: AssetImage('assets/Frame3.jpg'))),
           SizedBox(
             height: 30,
           ),
-          Text('Информация о таблице'),
-          Text('Информация о таблице'),
-          Text('Информация о таблице'),
-          Text('Информация о таблице'),
+          Text(
+            'SEXY CHAKRA',
+            style: TextStyle(
+                fontFamily: 'TrajanPro3Regular',
+                foreground: Paint()..shader = linearGradient),
+          ),
+          Text(
+            'Информация об амулете',
+            style: TextStyle(
+                fontFamily: 'TrajanPro3Regular',
+                foreground: Paint()..shader = linearGradient),
+          ),
+          Text(
+            'Информация об амулете',
+            style: TextStyle(
+                fontFamily: 'TrajanPro3Regular',
+                foreground: Paint()..shader = linearGradient),
+          ),
+          Text(
+            'Информация об амулете',
+            style: TextStyle(
+                fontFamily: 'TrajanPro3Regular',
+                foreground: Paint()..shader = linearGradient),
+          ),
+          Container(
+              margin: EdgeInsets.all(11),
+              height: 43,
+              width: 155,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                gradient: LinearGradient(
+                  colors: [
+                    Color(0xFFFFC700),
+                    Color(0xFFFFC700),
+                    Color(0xFFFEDA5B),
+                    Color(0xFFFEE075),
+                  ],
+                  // stops: [0.0, 0.33, 0.65, 1.0],
+                ),
+                // shape: BoxShape.rectangle,
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                    color: Color(0x40000000),
+                    offset: Offset(0.0, 4.0),
+                    blurRadius: 4.0,
+                  ),
+                ],
+              ),
+              child: MaterialButton(
+                height: 100,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(11.0),
+                ),
+                textColor: Colors.black,
+                child: Text(
+                  'КУПИТЬ АМУЛЕТ',
+                  style: TextStyle(
+                    fontFamily: 'Khula-Regular',
+                  ),
+                ),
+                padding: EdgeInsets.all(8.0),
+                onPressed: () => ({
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => MatrixTable())),
+                }),
+              )),
         ],
       ),
     );
